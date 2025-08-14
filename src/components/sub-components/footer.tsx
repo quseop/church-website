fd97527b8ab993e358b1240d6524e79e7af97e7d
quseop <qusee@proton.me>
2025-08-14 11:16:33 +0200

    Footer changes

--- src/components/sub-components/footer.tsx
+++ src/components/sub-components/footer.tsx
@@ -5,7 +5,17 @@ import React from "react";
 
 export function Footer() {
     return (
-        <section className="h-[35vh] flex flex-col  items-center gap-10 py-10 justify-end py-5 text-white w-full bg-black">
+        <section className="flex flex-col  items-center gap-10 py-10 justify-end  text-white w-full bg-black">
+
+
+            <nav className="flex gap-5 text-xs font-light tracking-widest max-md:hidden">
+                <Link href="/about" className="hover:underline">About Us</Link>
+                <Link href="/live" className="hover:underline">Live</Link>
+                <Link href="/announcements" className="hover:underline">Announcements</Link>
+                <Link href="/tithes-offerings" className="hover:underline">Tithes & Offerings</Link>
+                <Link href="/resources" className="hover:underline">Resources</Link>
+                <Link href="/contact" className="hover:underline">Contact Us</Link>
+            </nav>
 
             <div className="flex gap-5">
                 <Link href="https://www.facebook.com/BlessPower/" target="_blank">
@@ -20,8 +30,8 @@ export function Footer() {
                 <Image src={"/osm.png"} width={100} height={50} className="mt-2" alt={"OSM Logo"} />
             </Link>
 
-            <p className=" text-sm font-light flex justify-center items-center gap-1">
-                <Copyright size={25} /> Original Seed Ministries - 2025
+            <p className=" text-sm font-light flex justify-center items-center gap-2">
+                <Copyright size={20} /> Original Seed Ministries - 2025
             </p>
         </section>
     )
